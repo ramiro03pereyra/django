5c6d3ee9f1f3e8ce4c3489c9f318f87973830959
ramiro pereyra <rama03pereyra@gmail.com>
2025-07-06 15:04:55 -0300

    creamos modelo familiar

--- mi_primer_app/urls.py
+++ mi_primer_app/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import path
-from .views import saludo, saludo_con_template
+from .views import saludo, saludo_con_template, crear_familiar
 
 urlpatterns = [
     path('hola-mundo-template/', saludo_con_template),
+    path('crear-familiar/<str:nombre>/', crear_familiar),
 ]
--- mi_primer_app/views.py
+++ mi_primer_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import Familiar
 # Create your views here.
 from django.http import HttpResponse
  
@@ -9,3 +9,14 @@ def saludo(request):
 def saludo_con_template(request):
     return render(request, "mi_primer_app/saludo.html")
 
+def crear_familiar(request, nombre):
+    if nombre is not None:
+        nuevo_familiar=Familiar(
+            nombre=nombre,
+            apellido="ejemplo apellido",
+            edad=30,
+            fecha_nacimiento="2003-12-09",
+            parentesco="primo"
+        )
+        nuevo_familiar.save()
+    return render(request, "mi_primer_app/crear_familiar.html", {"nombre":nombre})
